484c48609f849fc28d15ddc647363dc86c99325d
joel-frimpong <joel.frimpong25@compscihigh.org>
2025-03-26 17:52:53 +0000

    Hello

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(__dirname + "/public"))
+
 app.set("view engine", "ejs");
 
 app.use(express.json());
@@ -27,33 +29,42 @@ const Country = mongoose.model("Country", countrySchema, "Countries");
 // Create a POST route for "/add/country" that adds a country using the request body (3 points)
 // Use postman to add at least THREE different countries
 app.post("/add/country", async (req, res)=> {
-  const me = await new country ({
-    country: "Ghana",
-    flagURL: "",
-    population: 33.79000000,
-    officialLanguage: "Twi",
-    hasNuclearWeapons: false
+  const me = await new Country ({
+    country: req.body.country,
+    flagURL: req.body.flagURL,
+    population: req.body.population,
+    officialLanguage: req.body.officialLanguage,
+    hasNuclearWeapons: req.body.hasNuclearWeapons
   }).save()
-  res.json(me)
+  res.json(Country)
 })
 
 // Create a GET route for "/" that renders countries.ejs with every country from the Countries collection (1 point)
 app.get("/", async (req, res)=>{
-  const country = await Country.find({})
-    res.render("countries.ejs", {country})
+  const countries = await Country.find({})
+    res.render("countries.ejs", {countries})
 })
 // Go to countries.ejs and follow the tasks there (2 points)
 
 
 // Create a dynamic PATCH route handler for "/update/{name}" that modifies the population of the country specified in the path (3 points)
 // Test this route on post man
-
+app.patch("/country/:name", async (req, res) =>{
+  const response = await Country.findOneAndUpdate({country: req.params.name}, {population: req.body.population})
+  res.json(response)
+})
 
 
 
 
 // Create a DELETE route handler for "/delete/country" that deletes a country of your choice (3 points)
 // Test this route on post man
+app.delete("/country/:name", async (req, res)=>{
+  const repsonse = await Country.findOneAndDelete({
+    country: req.params.name
+  })
+  res.json(response)
+})
 
 
 async function startServer() {
